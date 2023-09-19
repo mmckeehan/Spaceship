a59c2cc318ae1b56adb8122ef36a236672061620
Mckeehan <mmckeeha@gmail.com>
2023-09-19 15:33:59 -0400

    Version 1 - Completed version 1 of the game

---
+++ Spaceship/Asteroid.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+
+namespace Spaceship
+{
+    internal class Asteroid
+    {
+        public Vector2 position = new Vector2 (600, 300);
+        public int speed;
+        public int radius = 59;
+        
+
+        // Asteroid Constructor
+        public Asteroid(int newSpeed)
+        {
+            Random rand = new Random();
+            speed = newSpeed;
+            position = new Vector2(1280 + radius, rand.Next(0, 721));
+        }
+
+        // Asteroid Update
+        public void astroidUpdate(GameTime gameTime)
+        {
+            float dt = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            position.X -= speed * dt;
+        }
+    }
+}
---
+++ Spaceship/Controller.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Spaceship
+{
+    internal class Controller
+    {
+        public List<Asteroid> asteroids = new List<Asteroid>();
+        public double timer = 2;
+        public double maxTime = 2;
+        public int nextSpeed = 240;
+        public bool inGame = false;
+        public double totalTime = 0;
+
+        // Update Function
+        public void conUpdate(GameTime gameTime)
+        {
+            if (inGame)
+            {
+                timer -= gameTime.ElapsedGameTime.TotalSeconds;
+                totalTime += gameTime.ElapsedGameTime.TotalSeconds;
+            }
+            else
+            {
+                KeyboardState kState = Keyboard.GetState();
+                if (kState.IsKeyDown(Keys.Enter))
+                {
+                    inGame = true;
+                    totalTime = 0;
+                    timer = 2;
+                    maxTime = 2;
+                    nextSpeed = 240;
+                }
+            }
+
+            if (timer <= 0)
+            {
+                asteroids.Add(new Asteroid(nextSpeed));
+                timer = maxTime;
+                if (maxTime > 0.5)
+                {
+                    maxTime -= 0.1;
+                }
+                if (nextSpeed < 720)
+                {
+                    nextSpeed += 4;
+                }
+            }
+        }
+    }
+
+}
